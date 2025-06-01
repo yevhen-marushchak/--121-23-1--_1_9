793182cd2b7fca5994d1ed01096974a000a6e541
CherniukVika <ch.vika2006@gmail.com>
2025-06-01 23:00:28 +0300

    Додала клас Exceptions

---
+++ Hospital.BLL/Exceptions.cs
@@ -0,0 +1,12 @@
+﻿namespace Hospital.BLL
+{
+    public class SlotTakenException : Exception
+    {
+        public SlotTakenException(string message) : base(message) { }
+    }
+
+    public class InvalidAppointmentTimeException : Exception
+    {
+        public InvalidAppointmentTimeException(string message) : base(message) { }
+    }
+}
