50cfdf3214345ca0260d416b729ea7af43600720
CherniukVika <ch.vika2006@gmail.com>
2025-06-01 22:58:10 +0300

    Додала клас AppointmentSettings

---
+++ Hospital.BLL/AppointmentSettings.cs
@@ -0,0 +1,9 @@
+﻿namespace Hospital.BLL
+{
+    public static class AppointmentSettings
+    {
+        public static readonly TimeSpan StartTime = new TimeSpan(8, 0, 0); // 8:00
+        public static readonly TimeSpan EndTime = new TimeSpan(18, 30, 0); // 18:30
+        public static readonly int[] AllowedMinutes = new int[] { 0, 30 }; // :00 або :30
+    }
+}
